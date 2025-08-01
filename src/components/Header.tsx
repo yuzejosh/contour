103c4780207282da854fe99acbdef307f16b59a3
yuzejosh <josherheng@gmail.com>
2025-08-02 09:30:33 +1000

    refactor: update next.config.ts for image optimization and modify Header link to external site

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header: React.FC = () => {
   return (
     <header className="bg-white dark:bg-gray-700 text-[#007FFF] dark:text-[#66b2ff] p-4 flex justify-center items-center shadow-sm">
       <div className="flex justify-center items-center">
-        <Link href="/">
+        <Link href="https://www.contoureducation.com.au/">
           <Image 
             src="/contour_education.png" 
             width={256} 
